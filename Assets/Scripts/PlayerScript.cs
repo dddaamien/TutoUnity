f409da719e38006a4bf863d11ee327cc0d35e1d9
Damien <dddaamien@hotmail.com>
2015-03-10 11:37:26 +0100

    Limite de l'espace de jeu

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,6 +25,14 @@ public class PlayerScript : MonoBehaviour
 		movement = new Vector2 (speed.x * inputX,
 		                     speed.y * inputY);
 
+        var dist = (transform.position - Camera.main.transform.position).z;
+        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftBorder, rightBorder), Mathf.Clamp(transform.position.y, topBorder, bottomBorder), transform.position.z);
+
         bool shoot = Input.GetButton("Fire1");
         shoot |= Input.GetButton("Fire2");
         if(shoot)
