f7b22b0f13570c5dbe44c1614a33f727a7bb84c1
Damien <dddaamien@hotmail.com>
2015-03-10 11:15:57 +0100

    Spawn
    Active les ennemis uniquement lorsqu'ils sont visibles

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,26 +1,63 @@
 ﻿using UnityEngine;
-using System.Collections;
+
 
 public class EnemyScript : MonoBehaviour 
 {
     private WeaponScript[] weapons;
+    private bool hasSpawn;
+    private MoveScript moveScript;
 
 	void Awake()
     {
         weapons = GetComponentsInChildren<WeaponScript>();
-
+        moveScript = GetComponent<MoveScript>();
     }
 	
+    void Start()
+    {
+        hasSpawn = false;
+        collider2D.enabled = false;
+        moveScript.enabled = false;
+        foreach(WeaponScript weapon in weapons)
+        {
+            weapon.enabled = false;
+        }
+    }
 	// Update is called once per frame
 	void Update () 
     {
-        foreach(WeaponScript weapon in weapons)
+        if (hasSpawn == false)
         {
-            if(weapon!=null && weapon.CanAttack)
+            if(renderer.IsVisibleFrom(Camera.main))
             {
-                weapon.Attack(true);
+                Spawn();
             }
         }
-	    
+        else
+        {
+            foreach(WeaponScript weapon in weapons)
+            {
+                if(weapon!=null && weapon.enabled && weapon.CanAttack)
+                {
+                    weapon.Attack(true);
+                }
+            }
+            if (renderer.IsVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
+            }
+        }   
 	}
+
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        collider2D.enabled = true;
+        moveScript.enabled = true;
+        foreach(WeaponScript weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
 }
