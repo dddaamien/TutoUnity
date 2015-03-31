83e1a70db7a62330f26ccb2b0831de10155d4e4c
Damien <dddaamien@hotmail.com>
2015-03-31 10:51:32 +0200

    Création du menu
    menu de démarrage du jeu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour 
+{
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(Screen.width/2, Screen.height/2,84,60),"Start"))
+        {
+            Application.LoadLevel("Stage1");
+        }
+    }
+
+}
